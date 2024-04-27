2cbcb8acec165b0f0feb0bf73aa6c31b129d29d8
ekworsham <worsham.keith@gmail.com>
2024-04-27 13:23:10 -0400

    added grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write ("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+
+        if (number >= 90)
+        {
+            Console.Write ("Your grade is A");
+        }
+        else if (number >= 80 && number < 90)
+        {
+            Console.Write ("Your grade is B");
+        }
+        else if (number >= 70 && number < 80)
+        {
+            Console.Write ("Your grade is C");
+        }
+        else if (number >= 60 && number < 70)
+        {
+            Console.Write ("Your grade is D");
+        }
+        else 
+        {
+            Console.Write ("Your grade is F");
+        }
     }
 }
