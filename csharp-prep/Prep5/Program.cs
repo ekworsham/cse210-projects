2c18c34193b6e89f192bc7577db029b6ec5491cf
ekworsham <worsham.keith@gmail.com>
2024-04-27 20:18:42 -0400

    added functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,36 @@
 using System;
-
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string username = PromptUserName();
+        int number = PromptUserNumber();
+        int numberSquared = SquareNumber(number);
+        DisplayResults(username, numberSquared);   
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName ()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Pleases enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+    static void DisplayResults(string username, int number)
+    {
+        Console.Write($"{username}, the square of your number is {number}");
     }
 }
